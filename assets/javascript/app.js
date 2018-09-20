27d936bffc3d8a42b0413419b683098208d9f6bf
The Singer <kayla312@harbornet.com>
2018-09-20 00:58:23 -0700

    added the space that the gifs would load into, still gotta get the gifs to show up but thats the next commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,18 @@
-$(document).ready(function() {
+// set uo the document so that it is ready for when it is called upon
+// $(document).ready(function() {
 
-    console.log("working");
-    var topics = [];
-});
+//     console.log("working");
+//     var topics = [];
+// });
+// // add in the connection to the API and the key to allow it to function within the parameters of the call
+// function displayFitnessGifs() {
+
+// 	var search= $(this).data("search");
+// 	console.log();
+
+// 	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=zjVgoI8NKBNje4CZBp1SB1AGl47tTgEG&limit=10";
+
+// 	console.log(queryURL);}
+// ***************************************************
+
+
