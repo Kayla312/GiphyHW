304b8d89d8231e503efe07878d07deb0125a9d31
The Singer <kayla312@harbornet.com>
2019-05-10 11:39:28 -0700

    changes the term load location from the HTML to JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,14 +19,25 @@
 $(document).ready(function() {
     console.log("working");
     //Array for searched topics to be added
-    var topics = [];
+    var topics = ["weights", "stretching", "yoga", "cardio"];
     
+    function displayButtons() {
+      $("#addedButtons").empty();
+      for (var i = 0; i < topics.length; i++) {
+        var a = $('<button class="btn searchBtn">');
+        a.attr("data-search", topics[i]);
+        a.text(topics[i]);
+        $("#addedButtons").append(a);
+      }
+    };
+    displayButtons();
         //Function with AJAX call to GIPHY; Q parameterc for API link set to search term, limit 10 results
       //Create div with respective still and animate image sources with "data-state", "data-still" and "data-animate" attributes
-         function displayFitnessGifs() {
+    function displayFitnessGifs() {
     
-        var x = $(this).data("data-type");
-        console.log(x);
+        var x = $('#addGif').attr("value");
+
+        console.log("variable x" + x );
     
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=zjVgoI8NKBNje4CZBp1SB1AGl47tTgEG&limit=10";
     
@@ -43,7 +54,7 @@ $(document).ready(function() {
                 // learned this from the crystal collector video THANK GAWD
                 for (var i = 0; i < results.length; i++) {
                 
-                var showDiv = $("<div class='col-md-4'>");
+                var showDiv = $("<div class='col-md-3'>");
     
                 var rating = results[i].rating;
                 var defaultAnimatedSrc = results[i].images.fixed_height.url;
@@ -52,12 +63,12 @@ $(document).ready(function() {
                 var p = $("<p>").text("Rating:" + rating);
     
                 showImage.attr("src", staticSrc);
-                showImage.addClass("fitnessGifs");
+                showImage.addClass("fitnessGifs niceBorder");
                 showImage.attr("data-state", "still");
                 showImage.attr("data-still", staticSrc);
                 showImage.attr("data-animate", defaultAnimatedSrc);
-                showDiv.append(p);
                 showDiv.append(showImage);
+                showDiv.append(p);
                 $("#loadedGifs").prepend(showDiv);
     
             }
@@ -65,47 +76,51 @@ $(document).ready(function() {
     }
     
       //Submit button click event takes search term from form input, trims and pushes to topics array, displays button
-        $("addGif").on("click", function(event) {
-            event.preventDefault();
-            var  newFitness = $("#fitnessInput").val().trim();
+        $("#searchNewGif").on("click", function() {
+            // event.preventDefault();
+            var  newFitness = $("#addGif").val().trim();
             topics.push(newFitness);
-            console.log(topics);
-            $("#fitnessInput").val('');
+            console.log("topics" + topics);
+            $("#addGif").val('');
             displayButtons();
           });
     
-      //Function iterates through topics array to display button with array values in "addedButtons" section of HTML
+      
+      
+    //Function iterates through topics array to display button with array values in "addedButtons" section of HTML
+    //*************DONE*************
+      //   function displayButtons() {
+      //   $("#addedButtons").empty();
+      //   for (var i = 0; i < topics.length; i++) {
+      //     var a = $('<button class="btn btn-outline-secondary">');
+      //     a.attr("data-search", topics[i]);
+      //     a.text(topics[i]);
+      //     $("#addedButtons").append(a);
+      //   }
+      // };
+      // // call Display Buttons
+      // displayButtons();
 
-    //   *************DONE*************
-        function displayButtons() {
-        $("#addedButtons").empty();
-        for (var i = 0; i < topics.length; i++) {
-          var a = $('<button class="btn btn-outline-secondary">');
-          a.attr("data-search", topics[i]);
-          a.text(topics[i]);
-          $("#addedButtons").append(a);
-        }
-      }
-    
-    
-      displayButtons();
-    
-      //Click event on button with class of btn executes displayFitnessGifs function
-      $(document).on("click", ".btn", displayFitnessGifs);
-    
-      //Click event on gifs with class of "fitness gifs" executes pausePlayGifs function
-      $(document).on("click", ".fitnessGifs", pausePlayGifs);
-    
-      //Function accesses "data-state" attribute and depending on status, changes image source to "data-animate" or "data-still"
-      function pausePlayGifs() {
-           var state = $(this).attr("data-state");
+    //Function accesses "data-state" attribute and depending on status, changes image source to "data-animate" or "data-still"
+    //*************DONE*************
+        function pausePlayGifs() {
+          var state = $(this).attr("data-state");
           if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
             $(this).attr("data-state", "animate");
           } else {
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
-      }
-    }
+          }
+        };
+
+
+
+
     
+      //Click event on button with class of btn executes displayFitnessGifs function
+      $(document).on("click", ".btn", displayFitnessGifs);
+    
+      //Click event on gifs with class of "fitness gifs" executes pausePlayGifs function
+      $(document).on("click", ".fitnessGifs", pausePlayGifs);
     });
